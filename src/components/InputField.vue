53b3740e98d34a44f2962838e4936f6038b8b7fc
Sue Carson <suecarson@gmail.com>
2022-10-16 20:03:48 -0500

    numeric input

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center ">
     <label for="field" class="text-gray-400 text-sm focus-within:text-black block"><slot></slot>
       <input
-          type="text"
+          type="number"
           :value="modelValue"
           @input="$emit('update:modelValue', $event.target.value)"
           class="text-center block border rounded focus:outline-none  focus:border-gray-900 w-16"/>
